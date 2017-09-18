c75e69c9db3320d68599e38f700e5e5e718f257d
gustavosb8 <gustavo_s.bastos@hotmail.com>
2017-09-18 09:51:39 -0300

    Reajuste no controller veiculo

--- src/main/java/com/oficina/controller/VeiculoController.java
+++ src/main/java/com/oficina/controller/VeiculoController.java
@@ -39,7 +39,7 @@ public class VeiculoController {
     @GetMapping("/veiculos")
     public ModelAndView findAll() {
          
-        ModelAndView mv = new ModelAndView("/veiculos/controleveiculo.html");
+        ModelAndView mv = new ModelAndView("/veiculos/controleveiculo");
         List<Veiculo> veiculos = service.findAll();
         /*System.out.println("***** " + veiculos );*/
         
@@ -54,7 +54,7 @@ public class VeiculoController {
     @GetMapping("/veiculos/add")
     public ModelAndView add(Veiculo veiculo) {
  
-        ModelAndView mv = new ModelAndView("/veiculos/cadastroveiculo.html");
+        ModelAndView mv = new ModelAndView("/veiculos/cadastroveiculo");
         mv.addObject("veiculos", veiculo);
 
         return mv;
@@ -79,7 +79,7 @@ public class VeiculoController {
          
     	Veiculo veiculo = service.findOne(id_renavam );
 		
-		ModelAndView modelAndView = new ModelAndView("/veiculos/cadastroveiculo.html");
+		ModelAndView modelAndView = new ModelAndView("/veiculos/cadastroveiculo");
 		modelAndView.addObject("veiculo", veiculo);
 		
 		return modelAndView;
@@ -101,7 +101,7 @@ public class VeiculoController {
         
         if(validacao.hasErrors()) {
             
-        	return new ModelAndView("/veiculos/cadastroveiculo.html");
+        	return new ModelAndView("/veiculos/cadastroveiculo");
         }
         
         Modelo modelo = modeloService.findOne(idModelo);
